3214eb599261b5e36b7fe7a5ce499d17e1309bae
kchechet <36843888+kchechet@users.noreply.github.com>
2018-05-23 15:25:55 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,15 @@
+package ru.srqa.pft.sandbox;
+
 public class MyFirstProgram {
 	
    public static void main (String[]args){
        System.out.println("Hello World!");
+
+       System.out.println(2+2);
+       System.out.println();
+       System.out.println();
     }
+
+
 
 }
